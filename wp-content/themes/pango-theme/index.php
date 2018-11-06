83a6180a3a69b103badec7a9c24d960c229a36ba
Jason Tame <jason@pango.studio>
2018-11-06 14:30:08 +0200

    Make sure index.php cannot be accessed directly

--- wp-content/themes/pango-theme/index.php
+++ wp-content/themes/pango-theme/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -6,3 +7,10 @@
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
+
+ * @package Pango
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
